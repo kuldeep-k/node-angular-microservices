fe68e9e5f2b4494dcf57b015c65442e35b60ddc8
Kuldeep Kamboj <kuldeepk1979@gmail.com>
2023-06-19 00:39:28 +0530

    Angular deploy changes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    AUTH_API_HOST: "https://localhost:3000/auth",
-    CART_API_HOST: "https://localhost:3002/carts",
-    ORDER_API_HOST: "https://localhost:3002/orders",
-    PRODUCT_API_HOST: "https://localhost:3001/products"
+    AUTH_API_HOST: "http://localhost:3000/auth",
+    CART_API_HOST: "http://localhost:3002/carts",
+    ORDER_API_HOST: "http://localhost:3002/orders",
+    PRODUCT_API_HOST: "http://localhost:3001/products"
 };
